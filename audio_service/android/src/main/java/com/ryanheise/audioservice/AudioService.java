9d0751b8a7e918790393323b8cf3fc18196df5a7
Enrique Corpus <enriquecorpus@skydev.solutions>
2021-12-22 19:04:10 +0800

    force stop on task removed

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
@@ -698,6 +698,7 @@ public class AudioService extends MediaBrowserServiceCompat {
     public void onTaskRemoved(Intent rootIntent) {
         if (listener != null) {
             listener.onTaskRemoved();
+            listener.onStop();
         }
         super.onTaskRemoved(rootIntent);
     }
